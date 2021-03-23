ea5a6d9ff310be34fc9e9036fe2ccc197924affe
Mujhtech <mujeeb.muhideen@gmail.com>
2021-03-23 21:17:46 +0100

    add header to the page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_for_web/components/footer.dart';
+import 'package:flutter_for_web/components/header.dart';
+import 'package:flutter_for_web/utils/globals.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +15,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Web Portfolio',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      themeMode: ThemeMode.dark,
+      darkTheme: Theme.of(context).copyWith(
+        platform: TargetPlatform.android,
+        scaffoldBackgroundColor: Color(0xFFBA0000),
+        primaryColor: Colors.white,
+        canvasColor: Color(0xFFBA0000),
+      ),
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        ClampingScrollWrapper.builder(context, widget),
+        defaultScale: true,
+        breakpoints: [
+          ResponsiveBreakpoint.resize(450, name: MOBILE),
+          ResponsiveBreakpoint.resize(800, name: TABLET),
+          ResponsiveBreakpoint.resize(1000, name: TABLET),
+          ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+          ResponsiveBreakpoint.resize(2460, name: "4K"),
+        ],
+        background: Container(
+          color: Color(0xFFBA0000),
+        ),
       ),
       home: Home(),
     );
@@ -21,9 +44,66 @@ class MyApp extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xFFBA0000),
-      child: Column(),
+    return Scaffold(
+      key: Globals.scaffoldKey,
+      endDrawer: Drawer(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 24.0,
+            ),
+            child: ListView.separated(
+              itemBuilder: (BuildContext context, int index) {
+                return headerItems[index].isButton
+                    ? MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 28.0),
+                          child: TextButton(
+                            onPressed: headerItems[index].onTap,
+                            child: Text(
+                              headerItems[index].title,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 13.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    : ListTile(
+                        title: Text(
+                          headerItems[index].title,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return SizedBox(
+                  height: 10.0,
+                );
+              },
+              itemCount: headerItems.length,
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Container(
+            child: Header(),
+          ),
+          Footer(),
+        ]),
+      ),
     );
   }
 }
